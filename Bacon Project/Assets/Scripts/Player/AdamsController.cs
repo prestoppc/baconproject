1d4bf88bccfdea98f3ea9c87afb10e853eab3dff
Eric Peterford <epeterford@knights.ucf.edu>
2016-12-06 10:45:02 -0500

    Basic PlayerController setup.  AdamsController setup.  Adams- Aiming

---
+++ Bacon Project/Assets/Scripts/Player/AdamsController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AdamsController : MonoBehaviour {
+
+    private Camera mainCamera;
+    // Use this for initialization
+    void Start ()
+    {
+        mainCamera = FindObjectOfType<Camera>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
+
+        if (groundPlane.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+            Debug.DrawLine(cameraRay.origin, pointToLook, Color.blue);
+
+            transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+        }
+    }
+}
